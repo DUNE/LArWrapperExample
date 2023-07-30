1bd587f04f7050de2df36a4f20f69342123d955b
Heidi Schellman <33669005+hschellman@users.noreply.github.com>
2023-07-30 11:20:25 -0700

    scripts for job monitoring

---
+++ scripts/fd_mc_2023a_HD_status.sh
@@ -0,0 +1,13 @@
+echo prodgenie_anu_dune10kt_1x2x6.fcl
+metacat query -s "files where dune.campaign='fd_mc_2023a' and dune_mc.gen_fcl_filename='prodgenie_anu_dune10kt_1x2x6.fcl'"
+echo prodgenie_anue_dune10kt_1x2x6.fcl
+metacat query -s "files where dune.campaign='fd_mc_2023a' and dune_mc.gen_fcl_filename='prodgenie_anue_dune10kt_1x2x6.fcl'"
+echo prodgenie_anutau_dune10kt_1x2x6.fcl
+metacat query -s "files where dune.campaign='fd_mc_2023a' and dune_mc.gen_fcl_filename='prodgenie_anutau_dune10kt_1x2x6.fcl'"
+echo prodgenie_nu_dune10kt_1x2x6.fcl
+metacat query -s "files where dune.campaign='fd_mc_2023a' and dune_mc.gen_fcl_filename='prodgenie_nu_dune10kt_1x2x6.fcl'"
+echo prodgenie_nue_dune10kt_1x2x6.fcl
+metacat query -s "files where dune.campaign='fd_mc_2023a' and dune_mc.gen_fcl_filename='prodgenie_nue_dune10kt_1x2x6.fcl'"
+echo prodgenie_nutau_dune10kt_1x2x6.fcl
+metacat query -s "files where dune.campaign='fd_mc_2023a' and dune_mc.gen_fcl_filename='prodgenie_nutau_dune10kt_1x2x6.fcl'"
+#metacat query -s "files where dune.campaign='fd_mc_2023a' and dune_mc.gen_fcl_filename='prodgenie_nutau_dune10kt_1x2x6.fcl'"
